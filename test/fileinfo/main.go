d6824521e17bb7eec97c3fc28ee03d4574db780f
huoayi <hyk130406@icloud.com>
2023-10-31 19:45:41 +0800

    index

--- test/fileinfo/main.go
+++ test/fileinfo/main.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/sirupsen/logrus"
-	"net/url"
 	"os"
 	openapi "test-flag/openxpanapi"
 )
@@ -25,7 +24,7 @@ func init() {
 	apiClient = openapi.NewAPIClient(configuration)
 }
 func (req ShowDirInfoReq) ShowDirInfo() (*string, error) {
-	response, _, err := apiClient.FileinfoApi.Xpanfilelist(context.Background()).AccessToken(req.AccessToken).Dir(url.PathEscape(req.Path)).Execute()
+	response, _, err := apiClient.FileinfoApi.Xpanfilelist(context.Background()).AccessToken(req.AccessToken).Dir(req.Path).Execute()
 	if err != nil {
 		return nil, err
 	}
@@ -41,12 +40,15 @@ func main() {
 	var err error
 	fmt.Println("请输入 access_token")
 	req.AccessToken, err = input.ReadString('\n')
+	req.AccessToken = req.AccessToken[:len(req.AccessToken)-1]
 	fmt.Println("请输入 path 路径")
 	req.Path, err = input.ReadString('\n')
+	req.Path = req.Path[:len(req.Path)-1]
+	logrus.Infof("%+v", req)
 	info, err := req.ShowDirInfo()
 	if err != nil {
 		logrus.Error(err)
 		return
 	}
-	logrus.Info(info)
+	logrus.Info(*info)
 }
